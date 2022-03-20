884953528b7ecf7f657be596c3813eb3e12a6c96
felixdumaslavoie <felixdumaslavoie@gmail.com>
2022-03-19 22:09:01 -0400

    Structure globale complétée

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Collectif SPTS: un salaire pour toustes les stagiaires!</title>
-        <meta name="description" content="Collectif un salaire pour toustes les stagiaires" />
+        <meta name="description" content="Collectif SPTS: un salaire pour toustes les stagiaires" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -24,13 +24,13 @@ export default function Home() {
               <Link href="/materiel">
                 <a>Matériel</a>
               </Link></li>
-            <li className='feminist_color'><Link href="/historique">
+            <li className='nice_color'><Link href="/historique">
                 <a>Historique</a>
               </Link></li>
             <li className='sub1_color'><Link href="/evenements">
                 <a>Évenements</a>
               </Link></li>
-            <li className='feminist_color'><Link href="/archives">
+            <li className='nice_color'><Link href="/archives">
                 <a>Archives</a>
               </Link></li>
             <li className='sub1_color'><Link href="/appuis">
@@ -55,15 +55,17 @@ export default function Home() {
           <span>i</span>
           <span>f</span>
         </div>
+        <div className='header_mauve'>
           <span> </span>
           <span>S</span>
           <span>P</span>
           <span>T</span>
           <span>S</span>
           <span> </span>
+        </div>
         </h1>
         <h3>
-          Un salaire pour toustes les <div className='header_underline'>stagiaires!</div> 
+          Un <span className='detail_color'>s</span>alaire <span className='nice_color'>p</span>our <span className='base_color'>t</span>oustes les <div className='header_underline'><span className='detail_color'>s</span>tagiaires!</div> 
         </h3>
         </header>
 
