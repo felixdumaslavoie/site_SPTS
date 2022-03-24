863f26e4d9cdfe8388484b72e6ad5b8c33fe3419
felixdumaslavoie <felixdumaslavoie@gmail.com>
2022-03-24 11:55:59 -0400

    J'avance sur la fonctionalité de blog

--- pages/textes/[idTexte].js
+++ pages/textes/[idTexte].js
@@ -11,22 +11,23 @@ const Post = () => {
   var size = undefined;
 
   useEffect(() => {
-    if (Logo === undefined || endOfDocumentTop === undefined || size === undefined ) {
+    if (Logo === undefined || endOfDocumentTop === undefined || size === undefined) {
       Logo = document.getElementById("logo");
       endOfDocumentTop = 150;
       size = 0;
     }
 
-    window.onscroll = function() {
+    window.onscroll = function () {
       let scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    
+
       if (size == 0 && scroll > endOfDocumentTop) {
         Logo.className = 'smallLogo';
         size = 1;
-      } else if(size == 1 && scroll <= endOfDocumentTop){
+      } else if (size == 1 && scroll <= endOfDocumentTop) {
         Logo.className = 'largeLogo';
-       size = 0;
-      }};
+        size = 0;
+      }
+    };
 
   }, []);
 
@@ -34,45 +35,87 @@ const Post = () => {
   const router = useRouter()
   var texte = router.query;
   var id = undefined;
+  var item = undefined;
 
 
-  if (texte.idTexte !== undefined) 
-  {
+  if (texte.idTexte !== undefined) {
     id = texte.idTexte;
     window.$ = window.jQuery = require('jquery');
     var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSR_-ck57oRickpgHNsBAeXuCrLWcqiyGQFHsMNDevh8Oj2LZlfBlXbzYvk3XDdZbYr9kkURQy_ckrm/pub?gid=0&single=true&output=tsv";
+    
     $.ajax({
       url: url,
       context: document.body,
-      success: function(data){
-        
-        var item = data.split(" ");
-        
-        item.shift();
-        console.log(item)
-        $.each(item, function (i, ioo) {
-          let list_array = ioo.split("");
-          let listElement = '<li>' + '<a target="_blank" rel="noopener noreferrer" href="' + list_array[1] + '">' + list_array[0] + '</a>' + '</li>';
-          $('#appuis').append(listElement);
+      async: true,
+      cache: true,
+      success: function (data) {
+      
+        item = data.split(String.fromCharCode(9));
+
+        var finalArray = [];
+        const stringD = "^\r\n";
+        const stringF = "\r\n$";
+        const regexpDebut = new RegExp(stringD);
+        const regexpFin = new RegExp(stringF);
+        $.each(item, function (i, items) {
+          if (items.includes("\r\n")) {
+            if (regexpDebut.test(items) || regexpFin.test(items)) {
+              //console.log(items);
+              items = items.replace("\r\n", "");
+              finalArray.push(items);
+            }
+            else {
+              let tab = items.split("\r\n");
+              finalArray.push(tab[0]);
+              finalArray.push(tab[1]);
+            }
+          }
+          else if (items == "") { /* Do nothing...*/ }
+          else if (items) {
+            finalArray.push(items);
+          }
         });
+
+        finalArray.splice(0, 10) // On enlève les 10 premiers résultats
+
+        // Fonction de recherche du texte grâce à l'url de next.js...
+        var i = 1;
+        let trouve = false;
+        do {
+          if (finalArray[i]) {
+
+            if (finalArray[i] == texte.idTexte)
+            {
+              trouve = true;
+              break;
+            }
+            i += 5;
+          } else {
+            break;
+          }
+        } while (true);
+
+        //Garder juste l'information du texte recherché...
+        if (trouve)
+        {
+          i -= 1; // On revient une case avant pour bien se positionner...
+          finalArray = finalArray.splice(i, 5) // On coupe pour ne garder que le bon texte
+          $('meta[name=author]').attr('content', 'New Author Name');
+        }
       },
       error: function (err) {
         console.log(err.status);
-        console.log("erreur ajax appuis");
+        console.log("erreur dans la recherche du texte");
       }
-  });
-
-    
-     
-    $(function() {
-        
     });
   }
 
-  
 
-  
-  
+
+
+
+
+
 
 
   return (
@@ -80,9 +123,9 @@ const Post = () => {
 
       <div className={styles.container}>
         <Head>
-          <title>Collectif SPTS: Organismes appuyant notre lutte</title>
-          <meta property="og:title" content="Collectif SPTS: Organismes appuyant notre lutte" />
-          <meta property="og:url" content="https://collectifspts.org/texte" />
+          <title>Collectif SPTS: Textes</title>
+          <meta property="og:title" content="Collectif SPTS: Textes" />
+          <meta property="og:url" content="https://collectifspts.org/textes/" />
           <meta property="og:image" content="/thumbnails/appuis.jpg" />
           <meta property="og:type" content="website" />
           <meta property="og:description" content="Organismes qui appuient la salarisation" />
@@ -123,11 +166,11 @@ const Post = () => {
             <h1 className='headerh1blue'>
 
               <div className='header_underline'>
-                <span>A</span>
-                <span>p</span>
-                <span>p</span>
-                <span>u</span>
-                <span>i</span>
+                <span>T</span>
+                <span>e</span>
+                <span>x</span>
+                <span>t</span>
+                <span>e</span>
                 <span>s</span>
               </div>
             </h1>
