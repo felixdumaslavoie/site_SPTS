d64605e1d1e07196a02d0d30d3f7ba68cdf8013e
felixdumaslavoie <felixdumaslavoie@gmail.com>
2022-05-27 15:40:31 -0400

    Ajout de target_blank sur les liens des textes

--- pages/textes/[idTexte].js
+++ pages/textes/[idTexte].js
@@ -38,7 +38,29 @@ export async function getServerSideProps(context) {
   const publie = new Date(txt.result.data.attributes.Date);
   let laDate = date.format(publie, 'dddd D MMMM YYYY'); 
 
+  // Remember old renderer, if overridden, or proxy to default renderer
+  var defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  
+    // If you are sure other plugins can't add `target` - drop check below
+  var aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+      tokens[idx].attrPush(['target', '_blank']); // add new attribute
+      } else {
+        tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
+      }
+
+      // pass token to default renderer.
+      return defaultRender(tokens, idx, options, env, self);
+  };
+
   let contenu = md.render(txt.result.data.attributes.Contenu);
+
+  
 
   contenu = {__html: contenu};
 
